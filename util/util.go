9163a5e0c05fea2edf346a7cf21f8e90a2feee1a
Riotam <riotam.engineer@gmail.com>
2020-05-09 12:01:48 +0900

    add ABC085B and fix dir

--- util/util.go
+++ util/util.go
@@ -8,8 +8,8 @@ import (
 	"strings"
 )
 
-// getNumbersByStr はスペース区切りの文字列を、数値のスライスに変換して返す。
-func getNumbersByStr(s string) []int {
+// getSliceIntByString はスペース区切りの文字列を、数値のスライスに変換して返す。
+func getSliceIntByString(s string) []int {
 	var n []int
 	for _, f := range strings.Fields(s) {
 		i, err := strconv.Atoi(f)
@@ -17,35 +17,52 @@ func getNumbersByStr(s string) []int {
 			n = append(n, i)
 		}
 	}
-
 	return n
 }
 
 // getNextLine は標準入力された値を1行単位で文字列にして取得する。複数の実行で2行目、3行目と取得可能。
+// TODO: for文だと1.14では使えない？
 func getNextLine() string {
-
 	sc := bufio.NewScanner(os.Stdin)
 	sc.Scan()
 	return sc.Text()
-
 }
 
-// sumDigits は与えられた数値の各桁の和を求めて返す。
-func sumDigits(number int) int {
+// getLines は与えられた数値の回数だけ、標準入力された値を取得する。
+func getLines(n int) []string {
+	var lines []string
+	sc := bufio.NewScanner(os.Stdin)
+	for i := 0; i < n; i++ {
+		sc.Scan()
+		t := sc.Text()
+		lines = append(lines, t)
+	}
+	return lines
+}
+
+// getSumDigits は与えられた数値の各桁の和を求めて返す。
+func getSumDigits(number int) int {
 	remainder := 0
 	sum := 0
-
 	for number != 0 {
 		remainder = number % 10
 		sum += remainder
 		number = number / 10
 	}
-
 	return sum
 }
 
-// sortDesc は与えられたintのスライスを降順にソートして返す
-func sortDesc(slice []int) []int {
+// setSortDesc は与えられたintのスライスを降順にソートして返す
+func setSortDesc(slice []int) []int {
 	sort.Sort(sort.Reverse(sort.IntSlice(slice)))
 	return slice
+}
+
+func getSliceIntBySliceString(strings []string) []int {
+	var numbers []int
+	for _, s := range strings {
+		n, _ := strconv.Atoi(s)
+		numbers = append(numbers, n)
+	}
+	return numbers
 }
--- util/util_test.go
+++ util/util_test.go
@@ -6,10 +6,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_getNumbersByStr(t *testing.T) {
+func Test_getSliceIntByString(t *testing.T) {
 	s := "1 2 3"
 	want := []int{1, 2, 3}
-	got := getNumbersByStr(s)
+	got := getSliceIntByString(s)
 
 	if len(got) == 0 {
 		t.Errorf("failed test\nwant: %v\n got: %v", got, want)
@@ -23,13 +23,13 @@ func Test_getNumbersByStr(t *testing.T) {
 	}
 }
 
-func Test_sumDigits(t *testing.T) {
+func Test_getSumDigits(t *testing.T) {
 
 	numbers := []int{11, 10, 19, 29, 100, 199, 1000, 19999}
 	wants := []int{2, 1, 10, 11, 1, 19, 1, 37}
 
 	for i, n := range numbers {
-		got := sumDigits(n)
+		got := getSumDigits(n)
 
 		if wants[i] != got {
 			t.Errorf("failed test\nwant: %v\n got: %v", got, wants[i])
@@ -38,12 +38,22 @@ func Test_sumDigits(t *testing.T) {
 
 }
 
-func Test_sortDesc(t *testing.T) {
+func Test_setSortDesc(t *testing.T) {
 	target := []int{1, 2, 3, 4, 0}
-	sortDesc(target)
+	setSortDesc(target)
 	want := []int{4, 3, 2, 1, 0}
 
 	if !assert.Equal(t, target, want) {
 		t.Errorf("failed test\ntarget: %v\n  want: %v", target, want)
+	}
+}
+
+func Test_getSliceIntBySliceString(t *testing.T) {
+	target := []string{"1", "2", "3"}
+	got := getSliceIntBySliceString(target)
+	want := []int{1, 2, 3}
+
+	if !assert.Equal(t, got, want) {
+		t.Errorf("failed test\ntarget: %v\n   got: %v", target, got)
 	}
 }
